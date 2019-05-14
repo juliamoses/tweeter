04adb7968a23ffa35a4e12bce312fcde04dcbc1e
“Julia <jslmoses@gmail.com>
2019-05-14 22:22:09 +0000

    counter changed to red when invalid

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 
   $("#btn").on('input', function() {
     const counter = $(this).val().length;
-     $('.counter').text(maxValue - counter);
+    $('.counter').text(maxValue - counter);
+
+    $('.counter').text() < 0 ? $('.counter').addClass('invalid') : 
+    $('.counter').removeClass('invalid');
 });
 });
